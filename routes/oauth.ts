3bc774901059af159c017af781827a3fe08edabe
Maria Palacios Martinez <mpalaciosmartinez@mpalaci-ltmbbam.internal.salesforce.com>
2023-07-18 14:19:30 -0700

    adds utils and routes files

---
+++ routes/oauth.ts
@@ -0,0 +1,21 @@
+import 
+
+var client_id = 'd7527322ca104fe891303bb7837023e5';
+var redirect_uri = 'http://localhost:8888/callback';
+
+var app = express();
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
