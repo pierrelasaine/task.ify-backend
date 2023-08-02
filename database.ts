734d09c08859564dea41d5741fb54842b6b3301c
Camila Naranjo <cvnara25@colby.edu>
2023-08-01 18:24:36 -0700

    fixing package.json for heroku deployment

--- database.ts
+++ database.ts
@@ -7,6 +7,13 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  dialectOptions: {
+    // SSL is required to connect to a Heroku Postgres database
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // required to handle self-signed certificate issue with Heroku Postgres
+    }
+  }
 });
 
 export { sequelize };
